1b7125ed522f21ca5e2aa1750034ab3a00469a96
sr173 <66456804@qq.com>
2022-08-14 22:14:39 +0800

    m

--- nucvac.js
+++ nucvac.js
@@ -18,30 +18,61 @@ let now = (new Date().getTime())
 let sampleTimestamp = now - 1000 * 60 * 60 * 8
 let expireTimestamp = sampleTimestamp + 1000 * 60 * 60 * 48
 
+// body = JSON.stringify({
+//     "params": null,
+//     "errorMessage": null,
+//     "obj": {
+//         "expireFlag": false,
+//         "vaccineInfo": {
+//             "vaccinate": true,
+//             "idCard": "410482199803155051",
+//             "vaccinateCount": 3
+//         },
+//         "nucleicInfo": {
+//             "sampleTimestamp": sampleTimestamp,
+//             "expireTimeStamp": expireTimestamp,
+//             "samplingTime": new Date(sampleTimestamp).Format("yyyy-MM-dd HH:mm:ss"),
+//             "acidResult": "阴性",
+//             "sysTimeStamp": now,
+//             "timeDesc": "",
+//             "idCard": "410482199803155051",
+//             "expireTime": new Date(expireTimestamp).Format("yyyy-MM-dd HH:mm:ss"),
+//             "source": "0",
+//             "expireHour": 48
+//         },
+//         "idCard": "410482199803155051",
+
+//     },
+//     "success": true,
+//     "date": null,
+//     "version": null,
+//     "business": null,
+//     "requestId": null,
+//     "errorCode": null
+// })
+
 body = JSON.stringify({
     "params": null,
     "errorMessage": null,
     "obj": {
         "expireFlag": false,
         "vaccineInfo": {
-            "vaccinate": true,
-            "idCard": "410482199803155051",
-            "vaccinateCount": 3
-        },
-        "nucleicInfo": {
-            "sampleTimestamp": sampleTimestamp,
-            "expireTimeStamp": expireTimestamp,
-            "samplingTime": new Date(sampleTimestamp).Format("yyyy-MM-dd HH:mm:ss"),
-            "acidResult": "阴性",
-            "sysTimeStamp": now,
-            "timeDesc": "",
-            "idCard": "410482199803155051",
-            "expireTime": new Date(expireTimestamp).Format("yyyy-MM-dd HH:mm:ss"),
-            "source": "0",
-            "expireHour": 48
+            "vaccinate": false,
+            "vaccinateCount": 0
         },
         "idCard": "410482199803155051",
-
+        "nucleicInfo": {
+            "acidResult": "阴性",
+            "source": "0",
+            "idCard": "410482199803155051",
+            "sampleTimestamp": 1659667501000,
+            "sysTimeStamp": 1660486190801,
+            "expireTimeStamp": 1659883501000,
+            "expireHour": 48,
+            "samplingTime": "2022-08-05 10:45:01",
+            "expireTime": "2022-08-07 22:45:01",
+            "timeDesc": ""
+        }
     },
     "success": true,
     "date": null,
