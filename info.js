c0255a093325bc947362a93a188c86d92974dca9
admin <admin@DESKTOP-LD1Q4SL>
2022-11-18 19:29:45 +0800

    ggg

---
+++ info.js
@@ -0,0 +1,17 @@
+body = JSON.stringify({
+    "params": null,
+    "errorMessage": null,
+    "obj": {
+        "noticeFlag": "0",
+        "noticeCode": null,
+        "closeFlag": "1",
+        "cityCode": "zz"
+    },
+    "success": true,
+    "date": null,
+    "version": null,
+    "business": null,
+    "requestId": null,
+    "errorCode": null
+})
+$done({ body });
